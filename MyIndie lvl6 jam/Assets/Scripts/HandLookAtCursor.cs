d6172b4290268e7504162bc7f7999b189c32f96b
Ilya <ilya.zaharov.2009@mail.ru>
2025-10-17 20:06:46 +0300

    Добавел указатель рукой

---
+++ MyIndie lvl6 jam/Assets/Scripts/HandLookAtCursor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class HandLookAtCursor : MonoBehaviour
+{
+    [Header("Основные настройки")]
+    public Camera mainCamera;   
+    public Transform hand;        
+    public float distance = 10f;   
+
+    [Header("Оффсет поворота")]
+    public Vector3 rotationOffset; 
+
+    void Update()
+    {
+        if (mainCamera == null || hand == null)
+            return;
+
+        Vector3 mousePos = Input.mousePosition;
+
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, distance));
+
+        Vector3 direction = worldPos - hand.position;
+        direction.y = 0;
+
+        if (direction.sqrMagnitude > 0.001f)
+        {
+            Quaternion lookRot = Quaternion.LookRotation(direction);
+
+            hand.rotation = lookRot * Quaternion.Euler(rotationOffset);
+        }
+    }
+}
