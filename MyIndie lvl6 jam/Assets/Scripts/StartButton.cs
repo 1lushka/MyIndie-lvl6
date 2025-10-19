d43cc56bfe94a18186166155b7201d61a8f4ece5
1lushka <il.zah@mail.ru>
2025-10-19 13:26:49 +0300

    сделал кнопку стара в главном меню

---
+++ MyIndie lvl6 jam/Assets/Scripts/StartButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class StartButton : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onClick;
+    [SerializeField] private float pressDepth = 0.1f;
+    [SerializeField] private float pressDuration = 0.15f;
+
+    private bool isPressed = false;
+    private Vector3 originalPosition;
+
+    void Start()
+    {
+        originalPosition = transform.localPosition;
+    }
+
+    void OnMouseDown()
+    {
+        if (isPressed) return;
+        isPressed = true;
+
+        transform.DOLocalMoveY(originalPosition.y - pressDepth, pressDuration)
+            .SetEase(Ease.OutQuad)
+            .OnComplete(() =>
+            {
+                onClick?.Invoke();
+                transform.DOLocalMoveY(originalPosition.y, pressDuration).SetEase(Ease.InQuad);
+            });
+    }
+}
---
+++ MyIndie lvl6 jam/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Animator startAnimator;
+    [SerializeField] private string startTriggerName = "Start";
+    [SerializeField] private string gameSceneName = "GameScene";
+
+    [Header("Settings")]
+    [SerializeField] private float sceneLoadDelay = 1.5f;
+
+    private bool isStarting = false;
+
+    public void StartGameAnimation()
+    {
+        if (isStarting) return;
+        isStarting = true;
+
+        if (startAnimator != null)
+            startAnimator.SetBool(startTriggerName, true);
+
+        Invoke(nameof(StartGame), sceneLoadDelay);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+}
